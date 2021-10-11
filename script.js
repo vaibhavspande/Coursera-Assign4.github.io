623c10bbd9466e86e8b30a0f155d42c092627cad
Vaibhav Pande <91242365+vaibhavpande2605@users.noreply.github.com>
2021-10-11 14:13:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let str ="Hello";
+console.log(str + " Paul")
+console.log(str + " Frank")
+console.log(str + " Larry")
+console.log(str + " Paula")
+console.log(str + " Laura")
+console.log(str + " Yakkov")
+
+let greet = "Good bye "
+console.log(greet + "Larry")
+console.log(greet + "Paula")
+console.log(greet + "Laura")
+console.log(greet + "Yakko")
+
+
+
 
