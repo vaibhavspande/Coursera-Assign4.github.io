2891227e43825c8cc170415ea1d457176ebd8605
Vaibhav Pande <91242365+vaibhavpande2605@users.noreply.github.com>
2021-10-12 10:07:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-let str ="Hello";
-console.log(str + " Paul")
-console.log(str + " Frank")
-console.log(str + " Larry")
-console.log(str + " Paula")
-console.log(str + " Laura")
-console.log(str + " Yakkov")
-
-let greet = "Good bye "
-console.log(greet + "Larry")
-console.log(greet + "Paula")
-console.log(greet + "Laura")
-console.log(greet + "Yakko")
+var names=new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
-
-
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
+}
